d73222022ebe23af0fdc91ecadc538661ee36422
Dimitri Dhuyvetter <dimitri@dhuyvetter.eu>
2014-12-12 19:36:12 +0100

    code up to chapter 10

---
+++ tests/mocha.test.js
@@ -0,0 +1,8 @@
+describe('Mocha', function() {
+    describe('First Test', function() {
+        it('should assert 1 equals 1', function() {
+            expect(1).to.equal(1);
+        });
+    });
+});
+
